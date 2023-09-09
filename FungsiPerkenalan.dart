b8a3395897a9a7897868dcf798366dadb77d389e
MILA AMELIA <102271236+milaaaamelia@users.noreply.github.com>
2023-09-09 20:34:01 +0700

    Create FungsiPerkenalan.dart

---
+++ FungsiPerkenalan.dart
@@ -0,0 +1,17 @@
+class Mahasiswa {
+  String nim;
+  String nama;
+  int tahunLahir;
+
+  Mahasiswa(this.nim, this.nama, this.tahunLahir);
+
+  void perkenalan() {
+    int usia = DateTime.now().year - tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
+  }
+}
+
+void main() {
+  var mhs = Mahasiswa("H1D021008", "Mila Amelia", 2003);
+  mhs.perkenalan();
+}
